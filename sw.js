422328438b6f1a28b246bb30f47ef0682c491338
Sumarbrander <s80476@htw-dresden.de>
2021-06-09 02:04:47 +0200

    changed background color on mobile and fixed service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,30 @@
-self.addEventListener('install', event => event.waitUntil(
-  caches.open('pwa1').then(cache => cache.add('/'))
-));
+var cacheName = 'pwa';
+var filesToCache = [
+  '/BelegInternetTechnologien/',
+  '/BelegInternetTechnologien/index.html',
+  '/BelegInternetTechnologien/css/normalize.css',
+  '/BelegInternetTechnologien/css/main.css',
+  '/BelegInternetTechnologien/js/main.js',
+  '/BelegInternetTechnologien/js/plugins.js',
+  '/BelegInternetTechnologien/js/View/View.js',
+  '/BelegInternetTechnologien/js/Presenter/Presenter.js',
+  '/BelegInternetTechnologien/js/Model/Model.js',
+];
 
-self.addEventListener('fetch', event => event.respondWith(
-  caches.open('pwa1')
-    .then(cache => cache.match(event.request))
-    .then(response => response || fetch(event.request))
-));
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
